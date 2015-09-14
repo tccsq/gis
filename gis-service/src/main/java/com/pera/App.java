f842f197c8246fb43a0f68db0ee50f6304f74470
tccsq <tccsq@163.com>
2015-09-14 17:17:00 +0800

    first commit

---
+++ gis-service/src/main/java/com/pera/App.java
@@ -0,0 +1,13 @@
+package com.pera;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
